30bd6a9e80695bd0162eadde7233e8de6d9fb873
Bishvesh Tripathi <rajtripathi54671@gmail.com>
2024-04-30 23:04:26 +0530

    Dropdown in Searchbar functionality

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import '../header/header.css'
+import React from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import "../header/header.css";
+import Logo from "../../assets/images/logo.svg";
+import Select from "../selectDrop/select";
 
 
 const Header = () => {
   return (
-    <div>header</div>
-  )
-}
+    <header>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-2">
+            <img src={Logo} alt="err" />
+          </div>
+          {/* header search */}
+          <div className="col-sm-5">
+            <div className="headerSearch d-flex align-items-center">
+            <Select/>
+              
+              <div className="search">
+                <input type="text" placeholder="Search for items.."/>
+                <SearchIcon className="searchIcon cursor"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
